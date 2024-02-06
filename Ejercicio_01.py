8cf8f754dac482554e7db2d86bf6e6948f0a9bca
dvcandag <dvcandag@gmail.com>
2024-02-06 12:24:36 -0500

    Añadiendo ejercicio 01

---
+++ Ejercicio_01.py
@@ -0,0 +1,9 @@
+# Ingresar dos números
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+# Calcular la suma
+suma = numero1 + numero2
+
+# Imprimir el resultado
+print("La suma de", numero1, "y", numero2, "es:", suma)

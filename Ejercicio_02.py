e08af32bb37c81ae659f9a87405079866e0357e1
dvcandag <dvcandag@gmail.com>
2024-02-07 18:42:27 -0500

    Añadiendo ejercicio 02

---
+++ Ejercicio_02.py
@@ -0,0 +1,8 @@
+# Solicitar al usuario ingresar la temperatura en grados Celsius
+celsius = float(input("Ingrese la temperatura en grados Celsius: "))
+
+# Calcular la temperatura en grados Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Imprimir el resultado de la conversión
+print("La temperatura en grados Fahrenheit es:", fahrenheit)

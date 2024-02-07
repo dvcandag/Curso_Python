8408d85b8ece7ef5e5a96a5b0826c696b3a11c36
dvcandag <dvcandag@gmail.com>
2024-02-07 18:51:02 -0500

    Añadiendo ejercicio 03

---
+++ Ejercicio_03.py
@@ -0,0 +1,9 @@
+# Ingresar su año de nacimiento
+anio_nacimiento = int(input("Ingrese su año de nacimiento: "))
+
+# Calcular la edad actual
+anio_actual = 2024  # Asumiendo que el año actual es 2024
+edad = anio_actual - anio_nacimiento
+
+# Imprimir la edad actual
+print("Su edad actual es:", edad, "años.")

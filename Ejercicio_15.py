40afcd0fc5a9754c34ce5db973ad98aab01af58b
dvcandag <dvcandag@gmail.com>
2024-02-12 21:11:26 -0500

    Añadiendo ejercicio 15

---
+++ Ejercicio_15.py
@@ -0,0 +1,11 @@
+# Ejercicio 15: Calcular el área de un rectángulo
+
+
+ancho = float(input("Ingrese el ancho del rectángulo: "))
+altura = float(input("Ingrese la altura del rectángulo: "))
+
+# Calcula el área del rectángulo
+area = ancho * altura
+
+# Imprimir el resultado
+print("El área del rectángulo es:", area)

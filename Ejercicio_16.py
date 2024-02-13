78a3121feecf50ea24c2391708d46d1309cac9a7
dvcandag <dvcandag@gmail.com>
2024-02-12 21:15:30 -0500

    Añadiendo ejercicio 16

---
+++ Ejercicio_16.py
@@ -0,0 +1,11 @@
+# Ejercicio 16: Calcular el área de un círculo:
+
+
+#  ingresar el radio del círculo
+radio = float(input("Ingrese el radio del círculo: "))
+
+# Calcular el área del círculo
+area = 3.14159 * radio ** 2
+
+# Imprimir el resultado
+print("El área del círculo es:", area)

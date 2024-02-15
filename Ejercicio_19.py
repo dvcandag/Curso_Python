ca599a0a7f639c0fd5c09fba0d6d80ea1553cb89
dvcandag <dvcandag@gmail.com>
2024-02-14 22:02:45 -0500

    Añadiendo ejercicio 19

--- Ejercicio_19.py
+++ Ejercicio_19.py
@@ -5,6 +5,6 @@ nombre = input("Ingrese su nombre: ")
 edad = int(input("Ingrese su edad: "))
 
 if edad >= 18:
-    print(nombre, "ya eres mayor de edad.")
+    print(nombre, "Ya eres mayor de edad.")
 else:
-    print(nombre, "aun no eres mayor de edad.")
+    print(nombre, "Aun no eres mayor de edad.")

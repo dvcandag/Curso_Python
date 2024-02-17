1d30fd83829eb3aed743b47d150285e0547019d6
dvcandag <dvcandag@gmail.com>
2024-02-16 20:03:28 -0500

    Añadiendo ejercicio 24

---
+++ Ejercicio_24.py
@@ -0,0 +1,9 @@
+# Ejercicio 24: toma una letra como argumento y verifica si es una vocal
+
+def es_vocal(x):
+    if x == "a" or x == "e" or x == "i" or x == "o" or x == "u":
+        print("Es una vocal")
+    else:
+        print("No es una vocal")
+
+es_vocal("t") #argumento

1c844ba8985c8ecedd6461b57c3c604e6654a701
dvcandag <dvcandag@gmail.com>
2024-02-23 20:24:46 -0500

    Añadiendo ejercicio 38

---
+++ Ejercicio_38.py
@@ -0,0 +1,8 @@
+# Ejercicio 38: Muestre el eco de todo lo que el usuario introduzca 
+# hasta que el usuario escriba “salir” que terminará.
+
+while True:
+    frase = input("Introduce algo: ")
+    if frase == "salir":
+        break
+    print(frase)

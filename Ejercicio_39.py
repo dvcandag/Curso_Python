a636d25d64f4ccedf56d6fe35e4eafd7d5680df0
dvcandag <dvcandag@gmail.com>
2024-02-24 20:50:31 -0500

    Añadiendo ejercicio 39

---
+++ Ejercicio_39.py
@@ -0,0 +1,9 @@
+# Ejercicio 39: Hacer ejercicio que pida al usuario un número entero y muestre 
+# por pantalla un triángulo rectángulo como el de más abajo.
+
+n = int(input("Introduce la altura del triángulo (entero positivo): "))
+for i in range(1, n+1, 2):
+    for j in range(i, 0, -2):
+        print(j, end=" ")
+    print("")
+

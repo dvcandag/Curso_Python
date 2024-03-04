dbfe3ba580ee09bba09dea3867d4c8cae6cd891d
dvcandag <dvcandag@gmail.com>
2024-03-03 21:16:42 -0500

    Añadiendo ejercicio 49

---
+++ Ejercicio_49.py
@@ -0,0 +1,52 @@
+"""Escribir un programa que pregunte al usuario por las ventas de un rango de años y muestre 
+por pantalla un diagrama de líneas con la evolución de las ventas."""
+
+
+import matplotlib.pyplot as plt
+
+# Preguntamos por el año inicial
+while True:
+    try:
+        inicio = int(input('Introduce el año inicial: '))
+        break
+    except ValueError:
+        print("Por favor, introduce un año válido.")
+
+# Preguntamos por el año final
+while True:
+    try:
+        fin = int(input('Introduce el año final: '))
+        if fin >= inicio:
+            break
+        else:
+            print("El año final debe ser mayor o igual que el año inicial.")
+    except ValueError:
+        print("Por favor, introduce un año válido.")
+
+# Definimos un diccionario vacío para guardar las ventas de cada año
+ventas = {}
+
+# Bucle iterativo para preguntar las ventas de cada año y guardarlas en el diccionario
+for i in range(inicio, fin+1):
+    while True:
+        try:
+            ventas[i] = float(input('Introduce las ventas del año ' + str(i) +': '))
+            break
+        except ValueError:
+            print("Por favor, introduce un número válido.")
+
+# Definimos la figura y los ejes del gráfico con Matplotlib
+fig, ax = plt.subplots()
+
+# Dibujamos la línea con las ventas a partir del diccionario
+ax.plot(ventas.keys(), ventas.values())
+
+# Añadimos etiquetas
+ax.set_xlabel('Año')
+ax.set_ylabel('Ventas')
+
+# Añadimos título
+ax.set_title('Ventas por año')
+
+# Mostramos el gráfico por pantalla
+plt.show()

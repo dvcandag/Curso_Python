498eb4d4378b24a9474976f2c9045c8e79918be3
dvcandag <dvcandag@gmail.com>
2024-03-04 20:25:36 -0500

    Ejercicio de repaso - bucle

--- Ejercicio_50.py
+++ Ejercicio_50.py
@@ -1,1 +1,6 @@
-
+"""crear un programa que repita lo que el usuario escriba hasta que decida terminar ingresando "salir"."""
+while True:
+    frase = input("Introduce algo: ")
+    if frase == "salir":
+        break
+    print(frase)

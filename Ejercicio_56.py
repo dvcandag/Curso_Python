66b6fe32d2bbebc96be66dbcd468a3ecb5404e39
dvcandag <dvcandag@gmail.com>
2024-03-11 21:21:08 -0500

    Cargando el requerimiento

--- Ejercicio_56.py
+++ Ejercicio_56.py
@@ -1,1 +1,17 @@
+"""
+Ejercicio 56:
+Desarrollar una clase llamada "Cliente", diseñada para representar a un cliente de un banco y 
+permitir operaciones básicas de gestión de saldo. La clase debe tener los siguientes atributos y métodos:
+
+Atributos:
+a) nombre: Almacena el nombre del cliente.
+b) deposito: Guarda el saldo actual del cliente, inicialmente en 0.
+
+Métodos:
+a) __init__(self, nombre): Un constructor que inicializa el nombre del cliente y su saldo.
+b) imprimir(self): Un método que imprime el nombre y el saldo del cliente.
+c) depositar(self, monto): Permite al cliente depositar una cantidad específica.
+d) extraer(self, monto): Posibilita al cliente extraer una cantidad específica
+"""
+
 

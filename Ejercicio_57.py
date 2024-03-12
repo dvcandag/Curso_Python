8efdbe58ef2308a644f6258d6984d5d05cf4a57f
dvcandag <dvcandag@gmail.com>
2024-03-11 21:50:29 -0500

    Añadiendo ejercicio 57

--- Ejercicio_57.py
+++ Ejercicio_57.py
@@ -1,1 +1,17 @@
+"""
+Ejercicio 57:
+Se centra en la creación y manipulación de listas, explorando diversas formas de 
+inicialización y acceso a elementos. Se muestra la flexibilidad de las listas al permitir 
+la inclusión de elementos de diferentes tipos de datos en una sola lista.
+
+Instrucciones:
+1.- Analizar y comprender las distintas formas de inicializar listas, como asignación de valores 
+individuales, multiplicación de elementos y inclusión de elementos de diferentes tipos de datos.
+2.- Ejecutar el código proporcionado para observar los resultados de acceder a los elementos de cada lista.
+3.- Experimentar con la manipulación de listas, incluyendo la modificación de elementos existentes y la inclusión de nuevos elementos.
+4.- Reflexionar sobre las ventajas de utilizar listas y su aplicabilidad en diversos escenarios de programación.
+
+Proporcionar comentarios explicativos. Tu solución debe entregarse en un archivo .py
+
+"""
 

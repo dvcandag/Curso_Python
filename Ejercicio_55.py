e05f729881d7a64abbf1205e08423ea911dac3f0
dvcandag <dvcandag@gmail.com>
2024-03-11 21:06:22 -0500

    Cargando el requerimirnto

---
+++ Ejercicio_55.py
@@ -0,0 +1,9 @@
+"""Ejercicio 55: 
+
+Desarrolla una clase "Hipervinculo" en Python que represente un hipervínculo HTML 
+con dos atributos: "direccion" y "titulo". La clase debe incluir un método llamado 
+"retornar_hipervinculo" que devuelve el hipervínculo en formato HTML válido. Además, 
+crea una lista llamada "vector" que contendrá tres objetos de la clase Hipervinculo. 
+Asegúrate de seguir el estilo de codificación de Python, proporcionar comentarios 
+explicativos. Tu solución debe entregarse en un archivo .py"""
+

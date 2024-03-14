235e970f4781fd8a7908340eeb3bf38cc8fdba66
dvcandag <dvcandag@gmail.com>
2024-03-13 21:11:37 -0500

    Entregando ejercicio 57 - Listas

--- Ejercicio_57.py
+++ Ejercicio_57.py
@@ -15,3 +15,23 @@ Proporcionar comentarios explicativos. Tu solución debe entregarse en un archiv
 
 """
 
+# Creación de listas en Python
+vector1 = [None]  # Creación de una lista con un elemento None
+vector1[0] = 1  # Modificación del primer elemento de la lista
+
+vector2 = [None]  # Creación de otra lista con un elemento None
+vector2[0] = 6  # Modificación del primer elemento de la segunda lista
+
+vector3 = [None] * 5  # Creación de una lista con cinco elementos None
+vector3[0] = 4  # Modificación del primer elemento de la tercera lista
+
+vector4 = [3, 70, 'Vitto Andagua']  # Creación de una lista con elementos 1, 70 y 'juan'
+vector5 = [1, 70, 'Juan Lozada']  # Creación de otra lista con los mismos elementos que la anterior
+
+# Imprimir elementos de las listas
+print(vector1[0])  # Imprimir el primer elemento de la lista vector1
+print(vector2[0])  # Imprimir el primer elemento de la lista vector2
+print(vector3[0])  # Imprimir el primer elemento de la lista vector3
+print(vector4[0])  # Imprimir el primer elemento de la lista vector4
+print(vector5[0])  # Imprimir el primer elemento de la lista vector5
+

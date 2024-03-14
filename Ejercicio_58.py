3b2e6a781b946e1ab9d60420673076f56dccd739
dvcandag <dvcandag@gmail.com>
2024-03-13 21:16:03 -0500

    Añadiendo ejercicio 58

---
+++ Ejercicio_58.py
@@ -0,0 +1,16 @@
+"""
+Ejercicio 58:
+Desarrollar programa en Python que registre los depósitos realizados por diferentes clientes en un banco. 
+El programa debe permitir ingresar el número de cliente y el monto depositado. Si un cliente ha depositado 
+más de dos veces, el programa debe mostrar un aviso. Al finalizar, el programa debe mostrar la cantidad total 
+de depósitos realizados y el total del monto depositado por todos los clientes. Además, debe indicar 
+cuál cliente ha realizado más depósitos.
+
+Instrucciones:
+1.- Solicita al usuario ingresar el número de cliente (entero mayor que 0) y el monto a depositar (entero mayor o 
+    igual que 0). El ingreso de datos finaliza cuando se ingresa el número de cliente 0.
+2.- Si un cliente ha depositado más de dos veces, muestra "Aviso: Este cliente ha depositado más de dos veces."
+3.- Al finalizar, muestra la cantidad total de depósitos realizados y el monto total depositado por todos los clientes.
+4.- Además, indica qué cliente ha realizado más depósitos.
+
+"""

0604b7ecd3ca0c66d0813577c258890d933ce4e6
dvcandag <dvcandag@gmail.com>
2024-03-15 21:10:49 -0500

    Añadiendo ejercicio 59 - Funcional

--- Ejercicio_59.py
+++ Ejercicio_59.py
@@ -1,1 +1,13 @@
+"""
+    Ejercicio 59:
+    Desarrollar un programa que reciba un diccionario con las asignaturas y las notas de un alumno
+    y devuelve otro diccionario con las asignaturas en mayúsculas y las calificaciones
+    correspondientes a las notas aprobadas.
+
+    Argumentos:
+    notas: Un diccionario que contiene las asignaturas como claves y las notas como valores.
+
+    Retorna:
+    Un nuevo diccionario con las asignaturas en mayúsculas y las calificaciones correspondientes a las notas aprobadas iguales o mayores a 12.
+    """
 

3d6b3b1e7778871604fd7f3dfea85103340fe092
dvcandag <dvcandag@gmail.com>
2024-03-15 21:24:45 -0500

    Añadiendo ejercicio 60 - Ficheros

--- Ejercicio_60.py
+++ Ejercicio_60.py
@@ -1,1 +1,11 @@
+"""
+Ejercicio 60:
+Desarrollar un software para administrar un directorio telefónico que contenga los nombres y números de teléfono 
+de los clientes de una empresa. El programa incluirá funciones para crear el archivo del directorio en caso de que 
+no exista, para consultar el número de teléfono de un cliente, agregar el número de teléfono de un nuevo cliente y 
+eliminar el número de teléfono de un cliente existente. El directorio se almacenará en un archivo de texto llamado 
+listin.txt, donde el nombre del cliente y su número de teléfono estarán separados por comas, y cada cliente 
+ocupará una línea individual.
+
+"""
 

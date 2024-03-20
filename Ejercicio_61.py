d42c287139c7e5879c11767ee08aa5a1a0d4a8a4
dvcandag <dvcandag@gmail.com>
2024-03-19 20:29:17 -0500

    Añadiendo ejercicio 61 - Remover ceros

--- Ejercicio_61.py
+++ Ejercicio_61.py
@@ -1,1 +1,9 @@
+"""
+Ejercicio 17: Remover Ceros
+Desarrollar una función denominada (removerCeros) la cual acepta un arreglo 
+de números como argumento y devuelve un nuevo arreglo que excluye todos 
+los ceros (0) del arreglo original [4, 0, 5, 0, 2, 0, 5]"
 
+Proporcionar comentarios explicativos. Tu solución debe entregarse en un archivo .py 
+que se ejecute en el terminal mostrando la salida: [4, 5, 2, 5]
+"""

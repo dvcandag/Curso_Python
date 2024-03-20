9363280024321254070ba543c1dd708d13a44080
dvcandag <dvcandag@gmail.com>
2024-03-19 20:36:04 -0500

    Añadiendo ejercicio 62 - Sumar intervalo del arreglo

--- Ejercicio_62.py
+++ Ejercicio_62.py
@@ -1,1 +1,14 @@
+"""
+Ejercicio 18: Sumar intervalo del arreglo.
 
+Crea una función denominada (sumarArreglo) que requiera tres argumentos: un arreglo de números, 
+la posición inicial y la posición final. La función debe devolver la suma de todos los números 
+dentro del rango especificado, incluyendo tanto la posición inicial como la posición final.
+
+Nota: Se puede asumir que la posición inicial será menor o igual que la posición final, y 
+que ambas estarán dentro de los límites del arreglo [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+
+Proporcionar comentarios explicativos. Tu solución debe entregarse en un archivo .py 
+que se ejecute en el terminal mostrando como salida 18
+
+"""

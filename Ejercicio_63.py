df814e116a202b13e61f8883569e3c984c2f38fe
dvcandag <dvcandag@gmail.com>
2024-03-19 20:53:24 -0500

    Añadiendo ejercicio 63 - Generar contraseña

--- Ejercicio_63.py
+++ Ejercicio_63.py
@@ -1,1 +1,17 @@
+"""
+Ejercicio 19: Generar contraseña
+
+Desarrollar una función denominada (password) que tome un string como entrada y 
+devuelva otro string con los siguientes cambios realizados:
+
+a.- Convertir las mayúsculas a minúsculas.
+b.- Eliminar los espacios en blanco.
+c.- Reemplazar el carácter 'a' por '4'.
+d.- Reemplazar el carácter 'e' por '3'.
+e.- Reemplazar el carácter 'i' por '1'.
+f.- Reemplazar el carácter 'o' por '0'."
+
+Proporcionar comentarios explicativos. Tu solución debe entregarse en un archivo .py 
+ejecutandose satisfactoriamente en el terminal de su entorno de desarrollo.
+"""
 

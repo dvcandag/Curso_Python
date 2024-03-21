8737ea6ab7aec14abae120ea4ac88e40c3ecb8c3
dvcandag <dvcandag@gmail.com>
2024-03-20 20:22:02 -0500

    Añadiendo ejercicio 64 - Operaciones matemáticas

--- Ejercicio_64.py
+++ Ejercicio_64.py
@@ -1,1 +1,12 @@
+"""
+Ejercicio 19: Operaciones matemáticas
+Vitto tiene N dólares. Luis tiene la mitad de la cantidad de dinero que Vitto posee, mientras que 
+José tiene la mitad de la suma de lo que Vitto y Luis tienen juntos. Se desea calcular el promedio
+de la cantidad de dinero que poseen entre los tres y redondearlo al número entero más cercano.
+
+
+Proporcionar comentarios explicativos. Tu solución debe entregarse en un archivo .py 
+ejecutandose satisfactoriamente en el terminal de su entorno de desarrollo.
+"""
+
 

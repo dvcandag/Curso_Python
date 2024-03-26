6efc72305a4bec5383d6590093fbf82eeabf7a21
dvcandag <dvcandag@gmail.com>
2024-03-25 20:12:34 -0500

    Añadiendo ejercicio 64 -Declaraciones condicionales y elif

--- Ejercicio_65.py
+++ Ejercicio_65.py
@@ -1,1 +1,10 @@
+"""
+Ejercicio 64: Declaraciones condicionales y elif
 
+Desarrolla un programa que, gradualmente, permita comprender el funcionamiento de las declaraciones 
+condicionales if, elif (abreviatura de "else if") y else en Python, facilitando así la comprensión 
+de las estructuras de control de flujo en la programación informática.
+
+Agregar comentarios explicativos. La solución debe ser entregada en un archivo .py que se ejecute 
+correctamente en el terminal de tu entorno de desarrollo.
+"""

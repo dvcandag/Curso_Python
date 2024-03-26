8056e501df66a1c3db3a56770d47a06c5f0928e0
dvcandag <dvcandag@gmail.com>
2024-03-25 20:15:58 -0500

    Entregando ejercicio 64 -Declaraciones condicionales y elif

--- Ejercicio_65.py
+++ Ejercicio_65.py
@@ -8,3 +8,22 @@ de las estructuras de control de flujo en la programación informática.
 Agregar comentarios explicativos. La solución debe ser entregada en un archivo .py que se ejecute 
 correctamente en el terminal de tu entorno de desarrollo.
 """
+
+# SOLUCION
+
+# Definición de la función principal
+def main():
+    # Solicitar al usuario que ingrese un número entero
+    numero = int(input("Ingresa un número entero: "))
+
+    # Condicional para verificar si el número es positivo, negativo o cero
+    if numero > 0:
+        print("El número es positivo.")
+    elif numero < 0:
+        print("El número es negativo.")
+    else:
+        print("El número es cero.")
+
+# Llamada a la función principal
+if __name__ == "__main__":
+    main()

2f50735512a0fc94f81375c6113ef642c40ab875
dvcandag <dvcandag@gmail.com>
2024-03-28 21:39:59 -0500

    Agregando ejercicio 66 - Calculadora

--- Ejercicio_66.py
+++ Ejercicio_66.py
@@ -1,1 +1,16 @@
+"""
+Ejercicio 66: Calculadora
+Desarrollar una aplicación GUI denominada "Calculadora" que opere como una calculadora básica. La interfaz de la aplicación incluirá:
 
+1.- Tres etiquetas identificadas como "Primer número", "Segundo número" y "Resultado".
+2.- Tres campos de entrada de texto (lineEdit), donde el campo correspondiente al "Resultado" será de solo lectura.
+3.- Seis botones designados para realizar operaciones aritméticas: suma (+), resta (-), multiplicación (*), división (/), 
+módulo (%) y reinicio (RESET). Al presionar el botón "RESET", se eliminarán los valores ingresados en los tres campos 
+de entrada de texto. Al seleccionar cualquier operador aritmético (+, -, *, / o %), solo el campo relevante se modificará 
+para mostrar el resultado de la operación.
+
+4.- nstalar la biblioteca PyQt5 usando pip en el terminal o símbolo del sistema:
+
+pip install PyQt5
+
+"""

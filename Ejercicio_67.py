33ac2e30301828e5a2370b3cf9530840365bacf6
dvcandag <dvcandag@gmail.com>
2024-03-29 21:24:08 -0500

    Entregando ejercicio 66 - Función superposicion()

--- Ejercicio_67.py
+++ Ejercicio_67.py
@@ -8,3 +8,26 @@ Proporcionar comentarios explicativos. Tu solución debe ejecutarse  ejemplos y 
 
 """
 
+#SOLUCION
+
+def superposicion(lista1, lista2):
+    # Iterar sobre cada elemento de la primera lista
+    for elemento1 in lista1:
+        # Iterar sobre cada elemento de la segunda lista
+        for elemento2 in lista2:
+            # Comprobar si hay al menos un elemento en común
+            if elemento1 == elemento2:
+                return True
+    # Si no se encuentra ninguna superposición, devolver False
+    return False
+
+# Ejemplo 1 - false
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [6, 7, 8, 9, 10]
+print(superposicion(lista1, lista2))  # Output: False
+
+
+# Ejemplo 2 - true
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [5, 6, 7, 8, 9]
+print(superposicion(lista1, lista2))  # Output: True

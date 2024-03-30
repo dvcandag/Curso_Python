24218159a54aa18cf5c5f711ce719774574bfe3c
dvcandag <dvcandag@gmail.com>
2024-03-29 21:18:37 -0500

    Añadiend ejercicio 67 - Función superposicion()

--- Ejercicio_67.py
+++ Ejercicio_67.py
@@ -1,1 +1,9 @@
+"""
+Ejercicio 66: Función superposicion()
+Definir una función superposicion() que tome dos listas y devuelva True si tienen al menos 1 miembro 
+en común o devuelva False de lo contrario. Escribir la función usando el bucle for anidado
+
+Proporcionar comentarios explicativos. Tu solución debe ejecutarse  ejemplos y entregarse en un archivo .py
+
+"""
 

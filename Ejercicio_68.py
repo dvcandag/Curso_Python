f310ca241282ec06d4b67509f43a49edd5a3b29d
dvcandag <dvcandag@gmail.com>
2024-03-30 21:01:54 -0500

    Añadiendo ejercicio 68 - Sistema Decimal a Romanos

--- Ejercicio_68.py
+++ Ejercicio_68.py
@@ -1,1 +1,9 @@
+"""
+Ejercicio 68: Sistema Decimal a Romanos
 
+Crear una clase en Python que convierta un número entero en su equivalente en números romanos. 
+
+Añadir comentarios para explicar el funcionamiento del código. 
+El programa debe estar contenido en un archivo .py que pueda ejecutarse
+
+"""

8a05ba82a4766c07cd4ab757b8e77dcda43a804a
dvcandag <dvcandag@gmail.com>
2024-03-30 21:25:56 -0500

    Entregando ejercicio 69 - Generación de subconjuntos

--- Ejercicio_69.py
+++ Ejercicio_69.py
@@ -9,3 +9,35 @@ Incluir comentarios para explicar el funcionamiento del código.
 El código de ejemplo proporcionado en "Entrada" debe estar contenido en un archivo .py que sea ejecutable,
 mostrando los subconjuntos generados.
 """
+
+# SOLUCIÓN
+
+class Subconjuntos:
+    def generar_subconjuntos(self, conjunto):
+        """
+        Genera todos los posibles subconjuntos únicos de un conjunto de números enteros distintos.
+
+        :param conjunto: La lista de números enteros distintos.
+        :return: Una lista de todos los subconjuntos únicos.
+        """
+        resultado = [[]]  # Inicializamos con el conjunto vacío como primer subconjunto
+        
+        for elemento in conjunto:
+            # Generamos nuevos subconjuntos agregando el elemento a cada subconjunto existente
+            nuevos_subconjuntos = [subconjunto + [elemento] for subconjunto in resultado]
+            # Agregamos los nuevos subconjuntos a la lista de resultado
+            resultado.extend(nuevos_subconjuntos)
+        
+        return resultado
+
+# Conjunto de entrada
+conjunto = [4, 5, 6]
+
+# Creamos una instancia de la clase Subconjuntos
+generador = Subconjuntos()
+
+# Generamos los subconjuntos
+subconjuntos = generador.generar_subconjuntos(conjunto)
+
+# Mostramos la salida requerida
+print("Salida:", subconjuntos)

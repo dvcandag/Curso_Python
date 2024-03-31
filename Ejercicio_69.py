78315d594fa57dde632b44607f69d5958b873aee
dvcandag <dvcandag@gmail.com>
2024-03-30 21:20:40 -0500

    Añadiendo ejercicio 69 - Generación de subconjuntos

--- Ejercicio_69.py
+++ Ejercicio_69.py
@@ -1,1 +1,11 @@
+"""
+Ejercicio 69: Generación de subconjuntos
 
+Desarrollar una clase en Python que genere todos los posibles subconjuntos únicos de un conjunto de números enteros distintos.
+1.- Entrada: [4, 5, 6]
+2.- Salida: [[], [6], [5], [5, 6], [4], [4, 6], [4, 5], [4, 5, 6]]
+
+Incluir comentarios para explicar el funcionamiento del código.
+El código de ejemplo proporcionado en "Entrada" debe estar contenido en un archivo .py que sea ejecutable,
+mostrando los subconjuntos generados.
+"""

f3e6478d4e5af98365b3c512ce969be868076fab
dvcandag <dvcandag@gmail.com>
2024-04-01 22:25:51 -0500

    Añadiendo ejercicio 71: clase - denominada "Rectangulo"

--- Ejercicio_71.py
+++ Ejercicio_71.py
@@ -1,1 +1,10 @@
+"""
+Ejercicio 71: clase - denominada "Rectangulo"
 
+Crear una clase en Python denominada "Rectangulo" que tenga atributos de base y altura, 
+y que incluya un método para calcular y devolver el área del rectángulo.
+
+Incluir comentarios para explicar el funcionamiento del código.
+El código de su solución "El área del rectángulo debe sesultar ser 15" y debe estar contenido en un archivo .py que sea ejecutable.
+
+"""

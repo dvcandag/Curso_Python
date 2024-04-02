d0a1f3a5059645227f5e76631422b491220358d7
dvcandag <dvcandag@gmail.com>
2024-04-01 21:48:08 -0500

    Añadiendo ejercicio 70: clase - identificación de un par de elementos

--- Ejercicio_70.py
+++ Ejercicio_70.py
@@ -1,1 +1,13 @@
+"""
+Ejercicio 70: clase - identificación de un par de elementos
+
+Crear una clase en Python que identifique un par de elementos (sus índices) dentro de una matriz dada, 
+cuya suma sea igual a un número objetivo especificado.
+
+
+Incluir comentarios para explicar el funcionamiento del código.
+El código de su ejemplo proporcionado debe estar contenido en un archivo .py que sea ejecutable.
+"""
+
+
 

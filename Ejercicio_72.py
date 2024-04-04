9b73b20c84f3d3df187162358af5e298b6cf0a2b
dvcandag <dvcandag@gmail.com>
2024-04-03 21:46:50 -0500

    Añadiendo ejercicio 72 - función len()

--- Ejercicio_72.py
+++ Ejercicio_72.py
@@ -1,1 +1,11 @@
+"""
+Ejercicio 72: función len()
+
+Crear una función que determine la extensión de una lista o cadena específica es una práctica valiosa, 
+a pesar de que Python ya incluya la función len() de manera predeterminada.
+
+Agregar comentarios del funcionamiento del código del ejemplo a desarrollar. Asegúrate de que 
+tu solución esté encapsulada en un archivo ejecutable con extensión .py
+
+"""
 

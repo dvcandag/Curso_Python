3b2ee3fc934a673799655ce29be7492059f542bc
dvcandag <dvcandag@gmail.com>
2024-04-03 21:55:48 -0500

    Añadiendo ejercicio 73: True / False

--- Ejercicio_73.py
+++ Ejercicio_73.py
@@ -1,1 +1,11 @@
+"""
+Ejercicio 73: True / False
+
+Crear una función que reciba un carácter y determine si es una vocal, 
+retornando True si lo es y False en caso contrario.
+
+Agregar comentarios del funcionamiento del código del ejemplo a desarrollar. Asegúrate de que 
+tu solución esté encapsulada en un archivo ejecutable con extensión .py
+"""
+
 

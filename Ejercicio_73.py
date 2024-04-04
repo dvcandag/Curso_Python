52bab95a889173be9910250201483dcb0285af72
dvcandag <dvcandag@gmail.com>
2024-04-03 21:57:32 -0500

    Entregando ejercicio 73: True / False

--- Ejercicio_73.py
+++ Ejercicio_73.py
@@ -9,3 +9,33 @@ tu solución esté encapsulada en un archivo ejecutable con extensión .py
 """
 
 
+#SOLUCIÓN
+
+def vocal(caracter):
+    """
+    Función que determina si un carácter es una vocal o no.
+    
+    Parámetros:
+    - caracter: carácter a evaluar
+    - Retorna: True si el carácter es una vocal, False en caso contrario.
+    """
+    # Convertimos el carácter a minúscula para hacer la comparación más sencilla
+    caracter = caracter.lower()
+    
+    # Comprobamos si el carácter es una vocal
+    if caracter in 'aeiou':
+        return True
+    else:
+        return False
+
+# Ejemplo
+if __name__ == "__main__":
+    caracter = input("Ingrese un carácter: ")
+    
+    if len(caracter) == 1:
+        if vocal(caracter):
+            print("El carácter ingresado es una vocal.")
+        else:
+            print("El carácter ingresado no es una vocal.")
+    else:
+        print("Debe ingresar exactamente un carácter.")

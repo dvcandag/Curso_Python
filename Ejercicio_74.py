8ecdf9aa67231bd6a24904ef4a11952b244fa59a
dvcandag <dvcandag@gmail.com>
2024-05-08 19:50:17 -0500

    Entregando la codificación del Ejercicio 74: Histograma

--- Ejercicio_74.py
+++ Ejercicio_74.py
@@ -1,5 +1,18 @@
 """
 Ejercicio 74: Histograma
-Definir un histograma procedimiento() que tome una lista de números enteros e imprima un histograma en la pantalla. Ejemplo: procedimiento([4, 9, 7]) debería imprimir lo siguiente:
-"""
+Definir un histograma procedimiento() que tome una lista de números enteros e imprima un histograma en la pantalla. 
+Ejemplo: procedimiento([3, 7, 5]) debería imprimir lo siguiente:
 
+***
+*******
+*****
+
+"""
+#SOLUCIÓN
+
+def histograma_procedimiento(lista_numeros):
+ for numero in lista_numeros:
+        print('*' * numero)
+
+# Ejemplo de uso
+histograma_procedimiento([3, 7, 5])

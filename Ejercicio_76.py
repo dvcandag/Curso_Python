097a51879faefccede67cc3d108e595865463aef
dvcandag <dvcandag@gmail.com>
2024-05-08 20:26:12 -0500

    Añadiendo Ejercicio 76: Programa de cálculo de edades

--- Ejercicio_76.py
+++ Ejercicio_76.py
@@ -1,1 +1,22 @@
+"""
+Ejercicio 76: Programa de cálculo de edades
 
+Escribe un programa en Python que realice las siguientes tareas:
+
+1.- Entrada de datos: El programa debe solicitar al usuario que ingrese el año actual.
+2.- Registro de personas: Luego, el programa debe permitir al usuario ingresar el nombre y el año de nacimiento 
+    de tres personas diferentes.
+3.- Cálculo de edades: Para cada persona registrada, el programa debe calcular la edad que cumplirá durante el año actual.
+4.- Presentación de resultados: Finalmente, el programa debe imprimir en pantalla el nombre de cada persona junto con la 
+    edad que cumplirá durante el año en curso.
+
+Instrucciones Adicionales:
+
+1.- Asegúrate de que el programa maneje correctamente las entradas del usuario, proporcionando mensajes de error claros 
+    en caso de entrada inválida.
+2.- Fomenta la modularidad en el diseño de tu programa. Considera cómo dividir las tareas en funciones más pequeñas para facilitar 
+    la comprensión y el mantenimiento del código.
+3.- Piensa en casos de prueba adicionales que podrían ayudarte a verificar la corrección y robustez de tu programa. Esto podría 
+    incluir situaciones como años negativos, nombres con caracteres especiales, etc.
+    
+"""

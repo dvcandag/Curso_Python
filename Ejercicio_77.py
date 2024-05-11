aa61d89ef137ed16ce071f47207d292f4535c0bd
dvcandag <dvcandag@gmail.com>
2024-05-10 20:22:34 -0500

    Cargando el Ejercicio 77: Registro de Edades

--- Ejercicio_77.py
+++ Ejercicio_77.py
@@ -1,1 +1,13 @@
+"""
+Ejercicio 77: Registro de Edades
 
+Escribe un programa en Python que realice las siguientes tareas:
+
+1.- Entrada de datos: El programa debe permitir al usuario ingresar el número de personas cuyas edades se registrarán.
+2.- Registro de edades: Luego, el programa debe solicitar al usuario que ingrese las edades de las personas una por una.
+3.- Análisis de edades: Después de registrar todas las edades, el programa debe determinar la edad mínima, la edad máxima 
+    y el promedio de las edades registradas.
+4.- Presentación de resultados: Finalmente, el programa debe imprimir en pantalla la edad mínima, la edad máxima y el promedio 
+    de las edades registradas.
+    
+"""

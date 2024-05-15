a5140d102122557455e9ac82264a5dcb03d1059a
dvcandag <dvcandag@gmail.com>
2024-05-14 19:42:14 -0500

    Cargando Ejercicio 78: Tipo de triángulo

--- Ejercicio_78.py
+++ Ejercicio_78.py
@@ -1,1 +1,7 @@
-
+"""
+Ejercicio 78: Tipo de triángulo
+Escribe un programa en Python que determine el tipo de triángulo basado en la longitud de sus lados. El programa debe 
+solicitar al usuario que ingrese la longitud de los tres lados del triángulo y luego determinar si el triángulo es equilátero 
+(todos los lados iguales), isósceles (dos lados iguales) o escaleno (todos los lados diferentes). El programa debe manejar 
+correctamente situaciones donde se ingresen valores no válidos para los lados del triángulo.
+"""

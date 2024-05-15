557792d2b71a598d161475314a0f03c1349efaaa
dvcandag <dvcandag@gmail.com>
2024-05-14 19:43:51 -0500

    Cargando Ejercicio 79: Año olímpico

--- Ejercicio_79.py
+++ Ejercicio_79.py
@@ -1,1 +1,8 @@
+"""
+Ejercicio 79: Año olímpico
 
+Desarrolla un programa en Python que determine si un año ingresado por el usuario es un año olímpico o no. Un año es 
+considerado olímpico si es divisible por 4, excepto aquellos que son divisibles por 100 pero no por 400. El programa 
+debe solicitar al usuario que ingrese un año y luego indicar si ese año es un año olímpico o no. Asegúrate de manejar 
+correctamente situaciones donde se ingresen valores no válidos para el año.
+"""

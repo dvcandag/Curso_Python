636000c16c2acee9b1bfe6a3d4fccb46ecd6a162
dvcandag <dvcandag@gmail.com>
2024-05-15 19:54:57 -0500

    Cargando Ejercicio 80: Mayor de edad

--- Ejercicio_80.py
+++ Ejercicio_80.py
@@ -1,1 +1,9 @@
+"""
+Ejercicio 80: Mayor de edad
+
+Crea un programa en Python que determine si una persona es mayor de edad o no, basado en la edad ingresada por el usuario. El programa debe 
+solicitar al usuario que ingrese su edad y luego determinar si es mayor o igual a 18 años, considerando esta como la mayoría de edad. El programa 
+debe imprimir un mensaje indicando si la persona es mayor de edad o no. Asegúrate de manejar correctamente situaciones donde se ingresen valores 
+no válidos para la edad, como letras o números negativos.
+"""
 

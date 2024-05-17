6544f3385627550205763b766bc481ed10fe3aeb
dvcandag <dvcandag@gmail.com>
2024-05-16 20:05:34 -0500

    Cargando el avance - Ejercicio 79: Año olímpico

--- Ejercicio_79.py
+++ Ejercicio_79.py
@@ -6,3 +6,12 @@ considerado olímpico si es divisible por 4, excepto aquellos que son divisibles
 debe solicitar al usuario que ingrese un año y luego indicar si ese año es un año olímpico o no. Asegúrate de manejar 
 correctamente situaciones donde se ingresen valores no válidos para el año.
 """
+# SOLUCIÓN
+
+def es_año_olímpico(año):
+    # Comprobamos si el año es divisible por 4 y no es divisible por 100, o si es divisible por 400
+    if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+        return True
+    else:
+        return False
+

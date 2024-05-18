86a3c6c496b9cea3b850a8830e882b865b88425e
dvcandag <dvcandag@gmail.com>
2024-05-17 19:28:35 -0500

    Entregando - Ejercicio 80: Mayor de edad

--- Ejercicio_80.py
+++ Ejercicio_80.py
@@ -7,3 +7,16 @@ debe imprimir un mensaje indicando si la persona es mayor de edad o no. Asegúra
 no válidos para la edad, como letras o números negativos.
 """
 
+# SOLUCIÓN
+
+def es_mayor_de_edad():
+    try:
+        edad = int(input("Por favor, ingresa tu edad: "))
+        if edad >= 18:
+            print("Eres mayor de edad.")
+        else:
+            print("Eres menor de edad.")
+    except ValueError:
+        print("Error: Por favor ingresa un valor numérico válido para la edad.")
+
+es_mayor_de_edad()

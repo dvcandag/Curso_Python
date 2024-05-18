53ecddd33628759d34c313b1d6dda99036868cd2
dvcandag <dvcandag@gmail.com>
2024-05-18 18:41:29 -0500

    Enviando solución del Ejercicio 82: nnúmeros del 1 a 10 al revés

--- Ejercicio_82.py
+++ Ejercicio_82.py
@@ -3,3 +3,8 @@ Ejercicio 82: nnúmeros del 1 a 10 al revés
 
 Imprima los números del 1 a 10 al revés utilizando el ciclo for
 """
+# SOLUCIÓN
+
+# Utilizando un ciclo for para imprimir los números del 10 al 1 al revés
+for i in range(10, 0, -1):
+    print(i)
